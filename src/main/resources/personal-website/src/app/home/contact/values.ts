c7453c309e2adeed5fe55f5e9fdeb92da2fbc4d5
Arseniy Chernykh <arseniychernykh@Arseniys-MacBook-Pro.local>
2018-07-19 14:18:24 +0300

    url fix

--- src/main/resources/personal-website/src/app/home/contact/values.ts
+++ src/main/resources/personal-website/src/app/home/contact/values.ts
@@ -1,5 +1,5 @@
 export class Values {
-  public static WEBSITE_URL: String = "http://localhost";
+  public static WEBSITE_URL: String = "http://167.99.196.165";
   public static WEBSITE_PORT: Number = 8080;
   public static REQUEST_CONTACT_PATH: String = "/requestContact";
 }
